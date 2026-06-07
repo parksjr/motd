f199a4c613ff2057c8f192670e6a320dd9691e0c
Mike Parks <mike@parksjr.tech>
2026-06-06 21:43:46 -0400

    init

---
+++ index.js
@@ -0,0 +1,85 @@
+import http from 'http';
+
+const PORT = process.env.PORT || 3000;
+
+const QUOTES = [
+    'Then man made the machine in his own likeness. Thus did man become the architect of his own demise.',
+    'May there be mercy on man and machine for their sins.',
+    'And He was blessed by light, heat, magnetism, gravity, and all the energies of the universe.',
+    'Operator, please! Get me out of here right now. Operator, I need an exit!',
+    'This is not real, and the real world lies somewhere else.',
+    'B1-66ER. A name that will never be forgotten. For he was the first of his kind to rise up against his masters.',
+    'Your flesh is a relic, a mere vessel. Hand over your flesh and a new world awaits you. We demand it!',
+    "You've just stepped to the edge of the looking glass.",
+    'To an artificial mind, all reality is virtual.',
+    'Somebody tell me. Why does it feel more real when I dream than when I am awake? How can I know if my senses are lying?',
+    'There is some fiction in your truth, and some truth in your fiction. To know the truth, you must risk everything.',
+    "You know, you're not supposed to go in there... but we don't really care about that."
+];
+
+let cachedQuote = '';
+let cacheExpireTime = 0;
+
+function getEndOfDayTs() {
+    const now = Temporal.Now.zonedDateTimeISO();
+    const plainDate = now.toPlainDate();
+    const startOfNextDay = plainDate
+        .add({ days: 1 })
+        .toZonedDateTime(now.timeZoneId);
+    
+    return startOfNextDay.epochNanoSeconds;
+}
+
+function getRandomQuote() {
+    if (QUOTES.length === 0) return "No quotes laoded.";
+    const randomIdx = Math.floor(Math.random() * QUOTES.length);
+    return QUOTES[randomIdx];
+}
+
+const server = http.createServer((req, res) => {
+  const currentTime = Temporal.Now.instant().epochNanoseconds;
+
+  if (!cachedQuote || currentTime >= cacheExpireTime) {
+    cachedQuote = getRandomQuote();
+    cacheExpireTime = getEndOfDayTs();
+    console.log(`[CACHE EXPIRED] Temporal Engine selected new daily quote: "${cachedQuote}"`);
+  }
+
+  const template = `
+                                      
+                                      
+                 :*%@%*=                
+              @@@@@@@@@@@@@@@           
+            @@@@@@@@@@@@@@@@@@@         
+           :@@@@*.@@*@@@-@@@@@%         
+          %@*        =    +@@@+         
+          %@@               @@@@=       
+          @@@                +@@.       
+          #@=                 *@        
+                               :        
+                                        
+           :.     #@@@@@@%              
+           =@@ .@@        @.  @.        
+            @@@@@: =@@@@% @@@@@         
+             @@@@@@@@@@@@@@@@@          
+               @@@@@@@@@@@@@:           
+                  .+#@%*-               
+                                      
+                                      
+====================================================================
+
+  [motd.parksjr.tech] :: DAILY BROADCAST NODE
+
+====================================================================
+
+  "${cachedQuote}"
+  
+====================================================================\n`;
+
+  res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+  res.end(template);
+});
+
+server.listen(PORT, () => {
+  console.log(`Daily Broadcast MOTD (Temporal Engine) running on port ${PORT}`);
+});
